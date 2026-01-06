a46edfa8a4be3eecd8bbcd7139d1e9bf2007b5d6
SuriyaThiru68 <suriyathiru666@gmail.com>
2026-01-06 11:34:26 +0530

    logo added due to error

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = ({ activeSection }) => {
         <div className="flex justify-between items-center py-2 lg:py-2">
           <div className="text-2xl lg:text-3xl font-bold">
             <a href="#home" onClick={(e) => { e.preventDefault(); scrollToSection('home'); }} className="hover:opacity-80 transition-opacity" aria-label="Scroll to Home section">
-              <img src="./src/components/assests/mylogo.png" alt="Portfolio Logo" className="h-10 lg:h-12 w-auto" />
+              <img src=".src\components\assests\portlogo.png" alt="Portfolio Logo" className="h-10 lg:h-12 w-auto" />
             </a>
           </div>
           
